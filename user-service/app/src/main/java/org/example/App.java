9138e849a19d796849d42893ff9e38912d059e8c
SRI-2004 <ss1160@srmist.edu.in>
2025-05-04 22:29:32 +0530

    Update getUser for login flow

--- user-service/app/src/main/java/org/example/App.java
+++ user-service/app/src/main/java/org/example/App.java
@@ -12,3 +12,8 @@ public class App {
         System.out.println(new App().getGreeting());
     }
 }
+public class UserService {
+    public String getUser() {
+        return "User from login flow";
+    }
+}
